26157098b13f1f35cde37701093b454c2a9280b3
Adam <29668672+adamshaver@users.noreply.github.com>
2018-06-10 14:38:29 -0500

    initial commit

---
+++ search-result-jk-nav.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         (Google) Search Results J/K Navigation
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  listen for j or k to be pressed on google search results page and then shift the selected result based on this
+// @author       Adam Shaver
+// @match        https://www.google.com/search?q=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var results = document.querySelectorAll('.g');
+    var selectedIndex = -1;
+
+    if (results.length > 0) {
+        document.onkeydown = function(e) {
+            var typingInSearchBox = document.getElementById('lst-ib').attributes['aria-haspopup'].value;
+            if (typingInSearchBox == "true") {
+                // don't do anything when typing
+                return;
+            }
+
+            switch(e.key.toUpperCase()) {
+                case "J":
+                    moveNext();
+                    break;
+                case "K":
+                    movePrev();
+                    break;
+            }
+        };
+    }
+
+    function moveNext() {
+        if (selectedIndex < results.length - 1) {
+            resetCurrentResult();
+            selectedIndex++;
+            highlightSelectedResult();
+        }
+    }
+
+    function movePrev() {
+        if (selectedIndex > 0) {
+            resetCurrentResult();
+            selectedIndex--;
+            highlightSelectedResult();
+        }
+    }
+
+    function resetCurrentResult() {
+        if (selectedIndex > -1 && selectedIndex < results.length) { // sanity check we're in bounds
+            results[selectedIndex].style.borderLeft = "";
+            results[selectedIndex].style.paddingLeft = "0";
+        }
+    }
+
+    function highlightSelectedResult() {
+        if (selectedIndex > -1 && selectedIndex < results.length) {
+            var res = results[selectedIndex];
+            res.style.borderLeft = "5px solid #1a73e8";
+            res.style.paddingLeft = "10px";
+            res.querySelectorAll('h3.r a')[0].focus();
+            res.scrollIntoViewIfNeeded();
+        }
+    }
+})();
